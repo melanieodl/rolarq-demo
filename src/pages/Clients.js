c3d6462c5123eb67cc6eecf27f68b6a935dfae1d
Melanie Orellana <melanievane96@gmail.com>
2020-09-10 18:46:00 -0600

    Nombre de cliente completo

--- src/pages/Clients.js
+++ src/pages/Clients.js
@@ -5,12 +5,11 @@ import Table from '../components/tables/EditableTb'
 const clients = props => {
   const columns =    [
      { title: 'Id', field: 'id', hidden: true },
-     { title: 'Nombres', field: 'firstName',
-       validate: rowData => ( typeof rowData.firstName != 'undefined'?
-                                  rowData.firstName.trim() === '' ?
+     { title: 'Nombres', field: 'name',
+       validate: rowData => ( typeof rowData.name != 'undefined' && rowData.name?
+                                  rowData.name.trim() === '' ?
                                       { isValid: false, helperText: 'Nombre es requerido' } : true
                                   : false)},
-     { title: 'Apellidos', field: 'lastName' },
      { title: 'Dirección', field: 'address'},
      { title: 'Email', field: 'email'},
      { title: 'Télefono', field: 'phoneNumber', width: 150}
