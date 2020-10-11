72b40fd6fdcf124d56d869db5fbe32ef0e748942
Melanie Orellana <melanievane96@gmail.com>
2020-10-11 17:31:36 -0500

    trying to fix cors problems

--- src/api.js
+++ src/api.js
@@ -2,5 +2,8 @@ import axios from 'axios'
 
 
 export default axios.create({
-    baseURL: `http://localhost:8080/api/`
+    baseURL: `/api/`,
+    proxy: {
+      host: "http://localhost:8080"
+    }
   })
