d3b84e5174da936e3d1e1f30d3bf1b431c559405
Melanie Orellana <melanievane96@gmail.com>
2020-10-15 16:32:48 -0500

    Updates

---
+++ precache-manifest.4de4b5adeebed584d14479394dc9dc6d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94b491f070a6512600854b76d3404324",
+    "url": "/rolarq-demo/index.html"
+  },
+  {
+    "revision": "bc27031e90497ea7923b",
+    "url": "/rolarq-demo/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2ad778013ef4ebf06f72",
+    "url": "/rolarq-demo/static/js/2.c7cd4764.chunk.js"
+  },
+  {
+    "revision": "f53545a9b161ca36e03931c974ab4f59",
+    "url": "/rolarq-demo/static/js/2.c7cd4764.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc27031e90497ea7923b",
+    "url": "/rolarq-demo/static/js/main.cf8b5f5e.chunk.js"
+  },
+  {
+    "revision": "0b8da9601db99a74a52b",
+    "url": "/rolarq-demo/static/js/runtime-main.535ebcc2.js"
+  },
+  {
+    "revision": "61a8622eaeb628078e657acec1ca17b0",
+    "url": "/rolarq-demo/static/media/block.61a8622e.png"
+  },
+  {
+    "revision": "35aa61aae870792510e4bcce71a4dbb2",
+    "url": "/rolarq-demo/static/media/columna.35aa61aa.png"
+  },
+  {
+    "revision": "3a017e4d95c81a7973a175747531ffbf",
+    "url": "/rolarq-demo/static/media/imagotipo.3a017e4d.png"
+  },
+  {
+    "revision": "666d2fbb3b4125ba85a64db4b18fbfb3",
+    "url": "/rolarq-demo/static/media/isotipo.666d2fbb.png"
+  },
+  {
+    "revision": "66a4fa255220b49aba91d4eaf5d225c3",
+    "url": "/rolarq-demo/static/media/logotipo.66a4fa25.png"
+  }
+]);
